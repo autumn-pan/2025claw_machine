fe6ccb39a2f7741106ab0aa6006390f92bfd53a5
Autumn Pan <pan662607@gmail.com>
2025-03-08 14:43:08 -0500

    git commit 	new file:   .vscode/extensions.json 	new file:   .vscode/settings.json 	new file:   .vscode/vex_project_settings.json 	new file:   src/main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       autumn                                                       #
+# 	Created:      3/8/2025, 2:42:07 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
